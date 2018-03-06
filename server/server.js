729749fa003e69f30e70526f93b37fc2dd6d76c2
MAS <digitalcolony@gmail.com>
2018-03-06 12:29:13 -0800

    newMessage + createMessage

--- server/server.js
+++ server/server.js
@@ -9,14 +9,20 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-io.on("connection", socket => {
-  console.log("New user connected");
-});
-
 app.use(express.static(publicPath));
 
 io.on("connection", socket => {
   console.log("New user connected");
+
+  socket.emit("newMessage", {
+    from: "dude@yahoo.com",
+    text: "from the server",
+    createdAt: 12345
+  });
+
+  socket.on("createMessage", message => {
+    console.log("createMessage:", message);
+  });
 
   socket.on("disconnect", () => {
     console.log("User was disconnected");
